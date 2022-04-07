c16b623a324a8828b1da88035711c2b8d862ebc8
Victor Aiyeola <aiyeolavictor@gmail.com>
2022-04-07 06:46:48 +0100

    ⚡: fixed dah shiiiii 😜

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../theme";
-import { Layout } from "components/layouts";
+import { ChakraProvider } from '@chakra-ui/react';
+
+import theme from '../theme';
+import { Layout } from 'components/layouts';
+import Fonts from 'components/Fonts';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme} resetCSS>
+      <Fonts />
       {Component.requireAuth ? (
         <Layout>
           <Component {...pageProps} />
